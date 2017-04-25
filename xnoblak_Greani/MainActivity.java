01a5ab457940e6ce66f9c184467803dcec1448a2
xnoblak <nicooblak@gmail.com>
2017-04-25 16:39:08 +0200

    more bugfix

--- xnoblak_Greani/MainActivity.java
+++ xnoblak_Greani/MainActivity.java
@@ -4,6 +4,7 @@ public void oldMethod(){
 int i = 7;
 }
 public void newMethod(){
+printf("Here is a bugfix aswell, WOW COOL!");
 printf("Like dis!");
 double noblak = 4;
 printf("Added this on my bugfix branch");
