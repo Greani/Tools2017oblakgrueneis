9f8d8cda7a1661534d01f2bf9c404b3a86228d56
Greani <dominik.grueneis@gmail.com>
2017-04-25 16:09:25 +0200

    "Reset of the file"

--- xnoblak_Greani/MainActivity.java
+++ xnoblak_Greani/MainActivity.java
@@ -1,13 +1,5 @@
 
 public class MainActivity {
-<<<<<<< HEAD
-public void newMethod(){
-}
-=======
 public void oldMethod(){
-int i; 
-int i = 7;
 }
-
->>>>>>> a589212c3015f2809b300d36f4f661f62a559e3b
 }
