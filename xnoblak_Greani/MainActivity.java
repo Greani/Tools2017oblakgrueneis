b065f36e5ce5f18c5a82fccf5a383ae7a2599121
juliahauptmann <julia.hauptmann@hotmail.de>
2017-04-27 15:09:32 +0200

    new commit

--- xnoblak_Greani/MainActivity.java
+++ xnoblak_Greani/MainActivity.java
@@ -3,13 +3,13 @@ public class MainActivity {
 public void oldMethod(){
 int i = 7;
 }
-public void newMethod(){
-printf("Here is a bugfix aswell, WOW COOL!");
+public void newerMethod(){
+printf("Here is a bugfix aswell, WOW SO NOT COOL!");
 printf("Like dis!");
 double noblak = 18;
-printf("xnoblak");
+printf(“knoblauch”);
 while(noblak <18){
-printf("noblak too young");
+printf("noblak too young for knoblauch“);
 }
 double noblak = 4;
 printf("Added this on my bugfix branch");
